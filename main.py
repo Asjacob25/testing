61969bc46efcf250f384622d246bf965e2026ed8
Ashley Jacob <128179385+Asjacob25@users.noreply.github.com>
2024-10-21 17:17:15 -0400

    stuff

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ def subtract(a, b):
 def divide(a, b):
     return a/b
 
-def subtracttwo(a):
-    return (a-2)
-
 if __name__ == "__main__":
     print("Add:", add(5, 3))
     print("Subtract:", subtract(5, 3))
+#stuff
